88a1cac72bd339db60c9af0ac0b55b0477752666
rmezzeluna-png <rmezzeluna@gmail.com>
2026-05-12 18:25:05 +0700

    class ibu

--- agregasiibu.cpp
+++ agregasiibu.cpp
@@ -1,3 +1,11 @@
 #ifndef IBU_H
 #define IBU_H
 #include <vector>
+
+class ibu {
+public: 
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "ibu \"" << nama << "\" ada\n";
